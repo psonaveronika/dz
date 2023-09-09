3c009a6ba8b6f69465ea7a7c18700822168bb419
psonaveronika <143877486+psonaveronika@users.noreply.github.com>
2023-09-09 13:33:55 +0300

    Add files via upload

--- dz1/dz3.py
+++ dz1/dz3.py
@@ -1,5 +1,5 @@
-x = int(input())
-y = int(input())
-z = int(input())
-k=(x ** 2 + y ** 2+ z ** 2) ** 0.5
+x = float(input())
+y = float(input())
+z = float(input())
+k = (x ** 2 + y ** 2 + z ** 2) ** 0.5
 print(k)
