2a7d1b6f3171dc94f0837aba0a2f6728c728105d
psonaveronika <143877486+psonaveronika@users.noreply.github.com>
2023-10-14 13:12:27 +0300

    Update task3.py

--- task3.py
+++ task3.py
@@ -1,8 +1,7 @@
 def a(n):
     f = [0, 1, 1]
-    if n > 3:
-        for _ in range(n - 2):
-            f.append(f[-2] + f[-1])
+    for _ in range(n - 2):
+        f.append(f[-2] + f[-1])
     return f[:n]    # При 2, 1 или 0
 
 
