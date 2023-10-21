0290eedb8e982fca88cbe34efae8ee75b381b4cd
psonaveronika <143877486+psonaveronika@users.noreply.github.com>
2023-10-21 13:29:52 +0300

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,11 +1,11 @@
-def ans(n: int) -> str:
-    if x in(12, 1, 2):
+def ans(n):
+    if n in(12, 1, 2):
             return 'зима'
-    if x in(3, 4, 5):
+    if n in(3, 4, 5):
             return 'весна'
-    if x in(6, 7, 8):
+    if n in(6, 7, 8):
             return 'лето'
-    if x in(9, 10, 11):
+    if n in(9, 10, 11):
             return 'осень'
     return 'Ошибка'
 
